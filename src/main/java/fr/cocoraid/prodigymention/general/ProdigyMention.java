102316ca511a3200c134f60145485b509a95f00c
Tominous <18189490+Tominous@users.noreply.github.com>
2020-01-18 12:23:25 +0000

    Update ProdigyMention.java

--- src/main/java/fr/cocoraid/prodigymention/general/ProdigyMention.java
+++ src/main/java/fr/cocoraid/prodigymention/general/ProdigyMention.java
@@ -71,11 +71,11 @@ public class ProdigyMention {
     }
 
     public void printDebugInformation() {
-        /*this.nativeExecutor.log(Level.INFO, "Session HEAD check: " + CheckMethods.directSessionServerStatus(new Gson()));
+        this.nativeExecutor.log(Level.INFO, "Session HEAD check: " + CheckMethods.directSessionServerStatus(new Gson()));
         this.nativeExecutor.log(Level.INFO, "Help page check: " + CheckMethods.mojangHelpPage());
         this.nativeExecutor.log(Level.INFO, "Xpaw check: " + CheckMethods.xpaw());
         this.nativeExecutor.log(Level.INFO, "Mojang offline mode: " + MOJANG_OFFLINE_MODE);
-        this.nativeExecutor.log(Level.INFO, "Check status: " + CHECK_SESSION_STATUS);*/
+        this.nativeExecutor.log(Level.INFO, "Check status: " + CHECK_SESSION_STATUS);
     }
 
 }
